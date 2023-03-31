cbbc7a2d4bac154a789d82fef1edc72b61bdb117
coolshoeshine <o.enriquez89@gmail.com>
2023-03-30 20:30:48 -0700

    Proof of concept code to init a character and print some info about it

--- Final-FantaC/Final-FantaC/Final-FantaC.cpp
+++ Final-FantaC/Final-FantaC/Final-FantaC.cpp
@@ -1,12 +1,47 @@
 ﻿// Final-FantaC.cpp : Defines the entry point for the application.
 //
-
 #include "Final-FantaC.h"
 
-using namespace std;
+
+
+typedef struct {
+	char name[255];
+	int attack;
+	int defense;
+	int hitPoints;
+	int manaPoints;
+	bool isEnemy;
+} Character;
+
+void PrintCharacter(Character character)
+{
+	printf("%s\n", character.name);
+}
+
+
+
+void InitBarbarian(Character* barb)
+{
+	strcpy(barb->name, "Barbarian");
+}
+
+
+
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	// intro text
+	printf("==================================================\n");
+	printf("Welcome to Final-FantaC!\nA ghetto ripoff of old Final Fantasy battle tactics written in C.\n");
+	printf("==================================================\n");
+
+
+	// create the player's characters
+	Character Barbarian;
+	InitBarbarian(&Barbarian);
+	PrintCharacter(Barbarian);
+
+
+
 	return 0;
 }
