2e87a15cf8549a7a22c08648c8d907d6971038b4
coolshoeshine <o.enriquez89@gmail.com>
2023-03-30 20:14:46 -0700

    initial creation of cmake project.  Ignored all visual studio temp files, and build output files

---
+++ Final-FantaC/Final-FantaC/Final-FantaC.cpp
@@ -0,0 +1,12 @@
+﻿// Final-FantaC.cpp : Defines the entry point for the application.
+//
+
+#include "Final-FantaC.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
